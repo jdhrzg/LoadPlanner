8f135af0e05047e8465c4324b33c55afc2d4f545
Jerod Herzog <jdhrzg@gmail.com>
2025-10-26 09:30:48 -0500

    Simplify tab CSS and get tab selection working.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
 function tabButtonClick(event) {
-  console.log(event.srcElement.id == "tab-button-1");
+  // Clear "active" class from all tab elements and tab-content elements
+  var tabButtons = Array.from(document.getElementsByClassName("tab-button"));
+  tabButtons.forEach((tabButton) => {
+    tabButton.classList.remove("active");
+  });
+
+  var tabContents = Array.from(document.getElementsByClassName("tab-content"));
+  tabContents.forEach((tabContent) => {
+    tabContent.classList.remove("active");
+  });
+
+  // Add "active" class to selected tab element and associated tab-content
+  event.srcElement.classList.add("active");
+
+  var associatedTabContent = document.getElementById(
+    `tab-content-${event.srcElement.id.split("-")[2]}`
+  );
+  associatedTabContent.classList.add("active");
 }
