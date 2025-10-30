8fd5dcbbc4426405cbad3ff3734dd9513e9d4585
Jerod Herzog <jdhrzg@gmail.com>
2025-10-30 08:29:25 -0500

    Add, style and make sidebar functional in each tab

--- src/main.js
+++ src/main.js
@@ -18,3 +18,30 @@ function tabButtonClick(event) {
   );
   associatedTabContent.classList.add("active");
 }
+
+function toggleSidebar(event) {
+  var sidebarContents = Array.from(
+    document.getElementsByClassName("sidebar-content")
+  );
+  var mainContents = Array.from(
+    document.getElementsByClassName("main-content")
+  );
+
+  sidebarContents.forEach((sidebarContent) => {
+    if (sidebarContent.classList.contains("active")) {
+      event.currentTarget.innerText = ">";
+      sidebarContent.classList.remove("active");
+
+      mainContents.forEach((mainContent) => {
+        mainContent.classList.remove("sidebar-active");
+      });
+    } else {
+      event.currentTarget.innerText = "<";
+      sidebarContent.classList.add("active");
+
+      mainContents.forEach((mainContent) => {
+        mainContent.classList.add("sidebar-active");
+      });
+    }
+  });
+}
