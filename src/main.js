345d673117fe616181c59fd6a60554d557e2c7ab
Jerod Herzog <jdhrzg@gmail.com>
2025-11-03 07:42:03 -0600

    Add sidebar group on document load, layout and initial style

--- src/main.js
+++ src/main.js
@@ -1,11 +1,45 @@
+window.onload = function () {
+  let tabContent1 = document.getElementById("tab-content-1");
+  for (let child of tabContent1.children) {
+    if (child.classList.contains("sidebar-content")) {
+      child.appendChild(addSidebarGroup(1));
+    }
+  }
+};
+
+function addSidebarGroup(groupNumber) {
+  let groupDiv = document.createElement("div");
+  groupDiv.id = `sidebar-group-${groupNumber}`;
+  groupDiv.className = "sidebar-group";
+
+  groupDiv.innerHTML = `
+  <div class='sidebar-group-header'>
+    <div class="sidebar-group-header-title">Group ${groupNumber}</div>
+    <button class="sidebar-group-header-add">+</button>
+  </div>
+  <div class='sidebar-group-list'>
+    <div class='sidebar-group-list-item'>
+      <label for='length'>L:</label>
+      <input type="text" id='length'></input>
+      <label for='width'>W:</label>
+      <input type="text" id='width'></input>
+      <label for='height'>H:</label>
+      <input type="text" id='height'></input>
+    </div>
+  </div>
+  `;
+
+  return groupDiv;
+}
+
 function tabButtonClick(event) {
   // Clear "active" class from all tab elements and tab-content elements
-  var tabButtons = Array.from(document.getElementsByClassName("tab-button"));
+  let tabButtons = Array.from(document.getElementsByClassName("tab-button"));
   tabButtons.forEach((tabButton) => {
     tabButton.classList.remove("active");
   });
 
-  var tabContents = Array.from(document.getElementsByClassName("tab-content"));
+  let tabContents = Array.from(document.getElementsByClassName("tab-content"));
   tabContents.forEach((tabContent) => {
     tabContent.classList.remove("active");
   });
@@ -13,17 +47,17 @@ function tabButtonClick(event) {
   // Add "active" class to selected tab element and associated tab-content
   event.srcElement.classList.add("active");
 
-  var associatedTabContent = document.getElementById(
+  let associatedTabContent = document.getElementById(
     `tab-content-${event.srcElement.id.split("-")[2]}`
   );
   associatedTabContent.classList.add("active");
 }
 
 function toggleSidebar(event) {
-  var sidebarContents = Array.from(
+  let sidebarContents = Array.from(
     document.getElementsByClassName("sidebar-content")
   );
-  var mainContents = Array.from(
+  let mainContents = Array.from(
     document.getElementsByClassName("main-content")
   );
 
