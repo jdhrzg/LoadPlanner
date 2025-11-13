14e54e10f1f5ecfbb6fd308c3a78bdb287d09022
Jerod Herzog <jdhrzg@gmail.com>
2025-11-12 21:29:26 -0600

    Hook up sidebar add group list item button

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ window.onload = function () {
   element.appendChild(addSidebarGroup());
 };
 
+// 
+// SECTION: Add Sidebar Group / Add Sidebar Group List Item
+//
 function addSidebarGroup() {
   let nextIdSuffix = getNextIdSuffixByClassName("sidebar-group", "number");
 
@@ -14,7 +17,7 @@ function addSidebarGroup() {
     `
     <div class='sidebar-group-header'>
       <div class="sidebar-group-header-title">Group ${nextIdSuffix}</div>
-      <button class="sidebar-group-header-add">+</button>
+      <button class="sidebar-group-header-add" onclick="sidebarAddGroupListItemClick(event)">+</button>
     </div>
     <div class='sidebar-group-list'>
     </div>
@@ -29,28 +32,16 @@ function addSidebarGroupListItem(groupHTMLDivElement) {
   let insertIntoSidebarGroupList = Array.from(groupHTMLDivElement.children).find(child => child.className.includes("sidebar-group-list"));
   if (insertIntoSidebarGroupList === undefined) return;
 
-  let sidebarGroupListItemLetter = getNextIdSuffixByClassName("sidebar-group-list-item", "letter");
-  let sidebarGroupListItemId = `sidebar-group-list-item-${sidebarGroupListItemLetter}`;
+  let sidebarGroupListItemIdSuffix = getNextIdSuffixByClassName("sidebar-group-list-item", "letter");
+  let sidebarGroupListItemIdPrefix = "sidebar-group-list-item";
 
-  insertIntoSidebarGroupList.appendChild(createNewSidebarGroupListItem(sidebarGroupListItemId));
+  insertIntoSidebarGroupList.appendChild(createNewSidebarGroupListItem(sidebarGroupListItemIdPrefix, sidebarGroupListItemIdSuffix));
 }
 
-function getNextIdSuffixByClassName(className, letterOrNumber) {
-  let largestSidebarGroupNumber = undefined;
-  for (let element of document.getElementsByClassName(className)) {
-    let sidebarGroupNumber = element.id.split("-").at(-1);
-    if (sidebarGroupNumber > largestSidebarGroupNumber) {
-      largestSidebarGroupNumber = sidebarGroupNumber;
-    }
-  }  
-  if (largestSidebarGroupNumber === undefined) return (letterOrNumber == "letter") ? "A" : 1;
-  else return largestSidebarGroupNumber;
-}
-
-function createNewSidebarGroupListItem(sideBarGroupListItemId) {
+function createNewSidebarGroupListItem(sidebarGroupListItemIdPrefix, sidebarGroupListItemIdSuffix) {
   let newListItem = document.createElement("div");
-  newListItem.id = sideBarGroupListItemId;
-  newListItem.class = "sidebar-group-list-item";
+  newListItem.id = `${sidebarGroupListItemIdPrefix}-${sidebarGroupListItemIdSuffix}`;
+  newListItem.className = "sidebar-group-list-item";
 
   newListItem.innerHTML = `
   <label for='length'>L:</label>
@@ -59,11 +50,48 @@ function createNewSidebarGroupListItem(sideBarGroupListItemId) {
   <input type="text" id='width'></input>
   <label for='height'>H:</label>
   <input type="text" id='height'></input>
+  <div class="sidebar-group-list-item-label">${sidebarGroupListItemIdSuffix}</div>
   `;
 
   return newListItem;
 }
 
+function getNextIdSuffixByClassName(className, letterOrNumber) {
+  let largestSidebarGroupSuffix = "";
+  for (let element of document.getElementsByClassName(className)) {
+    let sidebarGroupSuffix = element.id.split("-").at(-1);
+    if (sidebarGroupSuffix > largestSidebarGroupSuffix) {
+      largestSidebarGroupSuffix = sidebarGroupSuffix;
+    }
+  }
+  
+  if (largestSidebarGroupSuffix === "") { 
+    return (letterOrNumber == "letter") ? "A" : 1;
+  } else if (letterOrNumber == "letter") {
+    let nextCharCode = 0;
+    let charCode = largestSidebarGroupSuffix.charCodeAt(0);
+    if (charCode >= 90) { // "Z"
+      nextCharCode = 97; // "a"
+    } else if (charCode >= 122) { // "z"
+      nextCharCode = 65; // "A"
+    } else {
+      nextCharCode = charCode + 1;
+    }
+    return String.fromCharCode(nextCharCode);
+  } else if (letterOrNumber == "number") {
+    return largestSidebarGroupSuffix + 1;
+  }
+}
+
+// Display suffix in UI, add remove button?
+function sidebarAddGroupListItemClick(event) {
+  addSidebarGroupListItem(event.currentTarget.parentNode.parentNode);
+}
+
+
+//
+// SECTION: Tab Click
+//
 function tabButtonClick(event) {
   // Clear "active" class from all tab elements and tab-content elements
   let tabButtons = Array.from(document.getElementsByClassName("tab-button"));
@@ -85,6 +113,10 @@ function tabButtonClick(event) {
   associatedTabContent.classList.add("active");
 }
 
+
+//
+// SECTION: Sidebar Toggle
+//
 function toggleSidebar(event) {
   let sidebarContents = Array.from(
     document.getElementsByClassName("sidebar-content")
